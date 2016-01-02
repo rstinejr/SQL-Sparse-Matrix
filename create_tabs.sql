90a87a9464716c71ece7a84f683f38661bbae9a3
rstinejr <bob@waltonstine.net>
2016-01-02 06:37:38 -0500

    Script to define and populate demo matrices.

---
+++ create_tabs.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Left 
+(
+	'i'   INT NOT NULL,
+	'j'   INT NOT NULL,
+	'val' INT NOT NULL
+);
+
+INSERT INTO Left
+VALUES
+    (1, 3, 27),
+	(1, 4, 13),
+	(1, 5, -15),
+	(3, 1, 177),
+	(3, 3, 225),
+	(3, 4, 55),
+	(4, 2, 3),
+	(4, 5, 88);
+
+CREATE TABLE Right
+(
+	'i'   INT NOT NULL,
+	'j'   INT NOT NULL,
+	'val' INT NOT NULL
+);
